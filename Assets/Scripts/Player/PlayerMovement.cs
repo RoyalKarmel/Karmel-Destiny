7febe093bfbc88d35918935fe07d9279b58a8536
Konrad <kondykox@gmail.com>
2024-04-20 21:40:43 +0200

    player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical) * speed * Time.deltaTime;
+        transform.Translate(movement);
+
+        if (moveHorizontal < 0)
+            spriteRenderer.flipX = false;
+        if (moveHorizontal > 0)
+            spriteRenderer.flipX = true;
+    }
+}
